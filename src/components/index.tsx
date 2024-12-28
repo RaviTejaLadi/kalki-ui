d3ca3b8c9c448914fa88623004c0af230207c5fa
Ravi Teja <ladi.ravi884781@gmail.com>
2024-12-28 12:34:40 +0530

    Implement Accordion component with context, header, body, and utility mappings

--- src/components/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Test: React.FC = () => {
-  return <div className="border">Test</div>;
-};
-
-export default Test;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import './index.css';
 
-export { default as Test } from './components';
+export {
+  default as Accordion,
+  AccordionHeader,
+  AccordionBody,
+  AccordionItem,
+} from './components/Accordion';
+
+export type {
+  AccordionVariant,
+  AccordionSize,
+  AccordionProps,
+  AccordionItemProps,
+  AccordionHeaderProps,
+  AccordionBodyProps,
+} from './components/Accordion';
